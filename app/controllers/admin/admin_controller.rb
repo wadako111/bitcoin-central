2b6b5dd3405fdb52da35b054638cde05b7adec9c
koichiro wada <wadako111@gmail.com>
2014-07-11 12:19:56 +0900

    off admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,11 +2,11 @@ class Admin::AdminController < ApplicationController
   before_filter :enforce_management_rights
 
   def enforce_management_rights
-    enforce_user_type(Manager)
+#     enforce_user_type(Manager)
   end
   
   def enforce_admin_rights
-    enforce_user_type(Admin)
+#     enforce_user_type(Admin)
   end
   
   def enforce_user_type(klass)
